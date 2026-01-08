cdd82d6743f6af5c82085f8f4064eef95c0081ae
Kirk Wang <k3wang@gmail.com>
2026-01-08 13:34:26 -0800

    🎁 Support PITT-IR's legacy URLs with redirects
    This commit is a stop gap measure for pittir tenant's legacy URLs.  This
    will allow users to go to /id/eprint/12345 or /12345 and be redirected
    to the correct /concern/work_types/:id URL.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 HykuKnapsack::Engine.routes.draw do
   mount Hyrax::Engine, at: '/'
+
+  # pittir Admin Note format: /id/eprint/12345
+  get '/id/eprint/:legacy_id', to: 'legacy_redirects#show', constraints: { legacy_id: /\d+/ }
+
+  # pittir Identifier format: /12345
+  get '/:legacy_id', to: 'legacy_redirects#show', constraints: { legacy_id: /\d+/ }
 end
