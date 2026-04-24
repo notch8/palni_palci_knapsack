752dda44a490387b29db85aa83e254addcaebc0f
Max Kadel <max@notch8.com>
2026-04-24 12:27:05 +0200

    Add migration to correctly remove old field
    Previously improperly removed by removing a migration, rather than adding a new migration to remove it. This migration has also been added to Bulkrax. It is idempotent, it is being added here so it can be rolled out more quickly.
    
    Connected to #590

---
+++ db/migrate/20260424081537_remove_parents_from_bulkrax_importer_runs.rb
@@ -0,0 +1,11 @@
+# This is a duplicate of a migration added in Bulkrax in https://github.com/samvera/bulkrax/pull/1183
+# The migration is idempotent - adding it here allows us to deploy it to production more quickly. 
+class RemoveParentsFromBulkraxImporterRuns < ActiveRecord::Migration[5.2]
+  def up
+    remove_column :bulkrax_importer_runs, :parents, if_exists: true
+  end
+
+  def down
+    add_column :bulkrax_importer_runs, :parents, :text, array: true, default: "{}", unless_exists: true
+  end
+end
