28f80e2e8db6bb4f968caae1aa676cb6ef5337fa
Max Kadel <max@notch8.com>
2026-04-24 14:04:15 +0200

    Rubocop fixes

--- db/migrate/20260424081537_remove_parents_from_bulkrax_importer_runs.rb
+++ db/migrate/20260424081537_remove_parents_from_bulkrax_importer_runs.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 # This is a duplicate of a migration added in Bulkrax in https://github.com/samvera/bulkrax/pull/1183
-# The migration is idempotent - adding it here allows us to deploy it to production more quickly. 
+# The migration is idempotent - adding it here allows us to deploy it to production more quickly.
 class RemoveParentsFromBulkraxImporterRuns < ActiveRecord::Migration[5.2]
   def up
     remove_column :bulkrax_importer_runs, :parents, if_exists: true
