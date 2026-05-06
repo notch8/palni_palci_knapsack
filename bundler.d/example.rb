bc90d55c98a895710c71716de21e73b236e9543a
Kirk Wang <k3wang@gmail.com>
2026-05-06 14:42:34 -0700

    willow sword => 2bdd6b5c1ef4fd6b04ce9e5cf462b46be8f580b1

--- bundler.d/example.rb
+++ bundler.d/example.rb
@@ -11,5 +11,4 @@ ensure_gem "sentry-rails"
 ensure_gem "cancancan", "~> 3.0" # cancancan is bundling to v1.17.0 but we need at least 3.0
 override_gem "willow_sword",
   github: "notch8/willow_sword",
-  ref: "f86d8f58ef487144e533ac68ca2f6958681a2b0c"
-  
+  ref: "2bdd6b5c1ef4fd6b04ce9e5cf462b46be8f580b1"
