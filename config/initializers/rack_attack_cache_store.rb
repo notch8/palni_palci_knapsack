11b14a7466ec22107d9709ec6dd9a92f62752ac6
Max Kadel <max@notch8.com>
2026-08-21 18:37:08 +0200

    Fix rubocop Style/IfUnlessModifier on the rack_attack_cache_store initializer

--- config/initializers/rack_attack_cache_store.rb
+++ config/initializers/rack_attack_cache_store.rb
@@ -1,6 +1,4 @@
 # frozen_string_literal: true
 
 # Rails.cache flips per-tenant (Account#setup_tenant_cache); pin Rack::Attack to Redis so it can't get stuck on the EFS FileStore variant (Errno::ESTALE under load).
-if ENV['RAILS_CACHE_STORE_URL'].to_s.start_with?('redis')
-  Rack::Attack.cache.store = ActiveSupport::Cache::RedisCacheStore.new(url: ENV['RAILS_CACHE_STORE_URL'])
-end
+Rack::Attack.cache.store = ActiveSupport::Cache::RedisCacheStore.new(url: ENV['RAILS_CACHE_STORE_URL']) if ENV['RAILS_CACHE_STORE_URL'].to_s.start_with?('redis')
